265a1b58c94972f7f1ef88e61bfb4fd4b5aa0e81
jhonMontano <jhon.montano@correo.tdea.edu.co>
2025-05-17 15:16:46 -0500

    It's modify the register to add the urls photos

--- src/domain/models/Register.js
+++ src/domain/models/Register.js
@@ -1,13 +1,16 @@
-export default class register{
-    constructor(name, lastName, birthday, email, password, gender, preferences, location, profilePhoto){
-        this.name = name, 
-        this.lastName = lastName,
-        this.birthday = birthday,
-        this. email = email,
-        this. password = password,
-        this.gender = gender,
-        this.preferences = preferences,
-        this.location = location,
-        this.profilePhoto = profilePhoto;
-    }
+export default class Register {
+  constructor({ id, name, lastName, birthday, email, password, gender, preferences, location, profilePhoto = [] }) {
+    this.id = id;
+    this.name = name;
+    this.lastName = lastName;
+    this.birthday = birthday;
+    this.email = email;
+    this.password = password;
+    this.gender = gender;
+    this.preferences = preferences;
+    this.location = location;
+    this.profilePhoto = Array.isArray(profilePhoto) ? profilePhoto : [profilePhoto];
+
+    console.log("🔧 Register class initialized:", this);
+  }
 }
